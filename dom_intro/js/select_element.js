a00d8cd0882dd608335247d5e84a2be1d10955f3
Sigrid van der Walt <91481317+Eagle747@users.noreply.github.com>
2022-05-20 22:32:51 +0200

    How to select an element through DOM

--- dom_intro/js/select_element.js
+++ dom_intro/js/select_element.js
@@ -1,10 +1,41 @@
-/* Select Elements  */
-/*                  */
-/* getElementById() */
+/* ***************************  */
+/*       Select Elements        */
+/*       getElementById()       */
+/* **************************** */
 
 const paragraph1 = document.getElementById('para1');
 const paragraph2 = document.getElementById('para2')
 
 console.log(paragraph1)
 console.log(paragraph2)
-console.log
+
+/* ***************************  */
+/*       Select Elements        */
+/* getElementById().textContent */
+/* **************************** */
+console.log('The content is accessed by: .textContent')
+console.log(paragraph1.textContent)
+console.log(paragraph2.innerText)
+console.log()
+
+/* ***************************  */
+/*       Select Elements        */
+/*       querySelector()        */
+/* **************************** */
+
+const h3Element = document.querySelector('h3')
+const list = document.querySelector('.list')
+console.log(h3Element)
+console.log(list)
+
+
+/* ***************************  */
+/*       Select Elements        */
+/*       querySelectorAll()        */
+/* **************************** */
+
+const listItems = document.querySelectorAll('ul>li')
+console.log(listItems)
+listItems.forEach(item => {
+  console.log(item.innerText)
+});
