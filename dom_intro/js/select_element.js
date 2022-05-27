c40455d8afd80be21d0ed9b1017c9de7eb9b8b80
Sigrid van der Walt <91481317+Eagle747@users.noreply.github.com>
2022-05-27 14:46:55 +0200

    DOM selecting an Element - select_element.html

---
+++ dom_intro/js/select_element.js
@@ -0,0 +1,10 @@
+/* Select Elements  */
+/*                  */
+/* getElementById() */
+
+const paragraph1 = document.getElementById('para1');
+const paragraph2 = document.getElementById('para2')
+
+console.log(paragraph1)
+console.log(paragraph2)
+console.log
